03158712ffb6e8fe5b0c95a38e2332118c663078
Mauro <mauroprogrammer7@gmail.com>
2023-08-14 19:40:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def main():
     return {"payload":"welcome to my project"}
 
+@app.route("/read")
+def read():
+    return {"payload":"read successfully"
+    }
+
 if __name__ == "main":
     app.run(debug=True)
